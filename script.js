03fe2165645653618268a40277d5cb6e13c3e847
urukrama kaushal <urukrama522407@gmail.com>
2024-11-05 14:39:08 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple alert on Buy Now button click
+document.querySelector('.cta').addEventListener('click', function() {
+    alert("Thank you for showing interest! Redirecting to purchase page...");
+});
